d5e0da45c6c90dd00faf14c5071465d06bf10474
ntdat723 <ntdat723@gmail.com>
2022-01-17 16:24:11 +0700

    rename function

--- Vector Space Model/UI_run.py
+++ Vector Space Model/UI_run.py
@@ -7,16 +7,16 @@ class ProjectUI(QMainWindow, Main_GUI):
     def __init__(self, parent=None):
         super(ProjectUI, self).__init__(parent)
         self.setupUi(self)
-        self.search_btn.clicked.connect(self.search_event)
+        self.search_btn.clicked.connect(self.on_button_clicked)
 
     # Populate list with data
-    def populate_list(self):
+    def populate_list(self, query):
         pass
 
     # on search button click
-    def search_event(self):
+    def on_button_clicked(self):
         if self.query_input.text() != "":
-            self.populate_list()
+            self.populate_list(self.query_input.text())
 
 
 if __name__ == '__main__':
